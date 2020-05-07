365d1bbc1ee07e818db6bb949fc8862e5cb2ad1f
Claudio Rivera <me@claudiorivera.com>
2020-05-07 15:06:44 -0500

    Fixed static route path

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 const gallery = require("./routes/gallery.router");
 
 const PORT = process.env.PORT;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(express.json());
-app.use(express.static("/client/build"));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use("/gallery", gallery);
